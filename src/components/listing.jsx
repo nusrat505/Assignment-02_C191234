dc8825d942888c26d9b617a4e882d7ae1e324a24
nusrat505 <148177940+nusrat505@users.noreply.github.com>
2023-12-25 11:07:31 +0600

    Add files via upload

---
+++ src/components/listing.jsx
@@ -0,0 +1,13 @@
+import IncomeList from "./incomelist";
+import ExpenseList from "./expenselist";
+
+export default function Listing() {
+  return (
+    <div
+      class="mx-auto my-8 grid max-w-4xl grid-cols-1 gap-8 px-5 text-sm md:grid-cols-2"
+    >
+        <IncomeList />
+        <ExpenseList />
+    </div>
+  );
+}
